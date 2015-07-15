c0699b29e76f0c3f0eb9a58cac6b3edb864895d0
Ramp <rampberrypi@gmail.com>
2015-07-15 08:24:08 -0400

    Started Changes For Snapshot

---
+++ src/main/webapp/resources/js/htmlsnips.js
@@ -0,0 +1,32 @@
+
+var HTMLSnips = {
+
+    createSnapshotPanel: function(){
+        
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="list-group">
+                        <a href="#" class="list-group-item active">Home</a>
+                        <div class="list-group-item">
+                        List header
+                        </div>
+                        <div class="list-group-item">
+                            <h4 class="list-group-item-heading">
+                            List group item heading
+                            </h4>
+                            <p class="list-group-item-text">
+                            ...
+                            </p>
+                        </div>
+                        <div class="list-group-item">
+                            <span class="badge">14</span>Help
+                        </div> <a class="list-group-item active"><span class="badge">14</span>Help</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    }
+
+
+}
